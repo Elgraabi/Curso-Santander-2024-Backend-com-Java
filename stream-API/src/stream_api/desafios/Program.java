17d205e8ddea5b670bc2b964863a51da53c369a8
Elgraabi <silvatdb8@gmail.com>
2024-05-29 16:14:10 -0300

    feat: desafio resolvido

--- stream-API/src/stream_api/desafios/Program.java
+++ stream-API/src/stream_api/desafios/Program.java
@@ -56,8 +56,8 @@ public class Program {
 
 		// Desafio 10 - Agrupe os valores ímpares múltiplos de 3 ou de 5:
 		System.out.println("Desafio 10 - Agrupe os valores ímpares múltiplos de 3 ou de 5:");
-		Map<String, List<Integer>> numerosMultiplosdeCincoOuTres = numeros.stream().filter(n -> n % 2 != 0).filter(m -> m % 3 == 0 || m % 5 == 0)
-				.collect(Collectors.groupingBy(n -> {
+		Map<String, List<Integer>> numerosMultiplosdeCincoOuTres = numeros.stream().filter(n -> n % 2 != 0)
+				.filter(m -> m % 3 == 0 || m % 5 == 0).collect(Collectors.groupingBy(n -> {
 					if (n % 3 == 0 && n % 5 == 0) {
 						return "Multiplos de 3 e 5";
 					} else if (n % 3 == 0) {
@@ -67,6 +67,63 @@ public class Program {
 					}
 				}));
 		numerosMultiplosdeCincoOuTres.forEach((key, value) -> System.out.println(key + ":" + value));
+
+		// Desafio 11 - Encontre a soma dos quadrados de todos os números da lista:
+		System.out.println("Desafio 11 - Encontre a soma dos quadrados de todos os números da lista:");
+
+		Integer m = numeros.stream().mapToInt(n -> n * n).sum();
+		System.out.println(m);
+
+		// Desafio 12 - Encontre o produto de todos os números da lista:
+		System.out.println("Desafio 12 - Encontre o produto de todos os números da lista:");
+
+		int x = numeros.stream().reduce(1, (a, b) -> a * b);
+		System.out.println(x);
+
+		// Desafio 13 - Filtrar os números que estão dentro de um intervalo:
+		System.out.println("Desafio 13 - Filtrar os números que estão dentro de um intervalo:");
+
+		System.out.println(numeros.stream().sorted().filter(n -> n >= 5 && n <= 10).toList());
+
+		// Desafio 14 - Encontre o maior número primo da lista:
+		System.out.println("Desafio 14 - Encontre o maior número primo da lista:");
+		System.out.println(numeros.stream().sorted().filter(Program::isPrime).max(Integer::compareTo));
+
+		// Desafio 15 - Verifique se a lista contém pelo menos um número negativo:
+		System.out.println("Desafio 15 - Verifique se a lista contém pelo menos um número negativo:");
+
+		numeros.stream().filter(n -> n < 0).forEach(System.out::println);
+
+		// Desafio 16 - Agrupe os números em pares e ímpares:
+		System.out.println("Desafio 16 - Agrupe os números em pares e ímpares:");
+		Map<Boolean, List<Integer>> agrupados = numeros.stream().collect(Collectors.partitioningBy(n -> n % 2 == 0));
+		List<Integer> pares = agrupados.get(true);
+		List<Integer> impares = agrupados.get(false);
+		System.out.println("Números pares: " + pares);
+		System.out.println("Números ímpares: " + impares);
+
+		// Desafio 17 - Filtrar os números primos da lista:
+		System.out.println("Desafio 17 - Filtrar os números primos da lista:");
+		numeros.stream().filter(Program::isPrime).forEach(n -> System.out.print(n + " "));
+
+		// Desafio 18 - Verifique se todos os números da lista são iguais:
+		System.out.println("\nDesafio 18 - Verifique se todos os números da lista são iguais:");
+
+		System.out.println(numeros.stream().distinct().count() == 1);
+
+		// Desafio 19 - Encontre a soma dos números divisíveis por 3 e 5:
+		System.out.println("Desafio 19 - Encontre a soma dos números divisíveis por 3 e 5:");
+		System.out.println(numeros.stream().filter(n -> n % 3 == 0 && n % 5 == 0).mapToInt(Integer::intValue).sum());
+
 	}
 
+	public static boolean isPrime(int num) {
+		if (num <= 1)
+			return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
 }
