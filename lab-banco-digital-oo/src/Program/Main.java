12b649121ba9cc09d6756b74364e6008e173ed51
Elgraabi <silvatdb8@gmail.com>
2024-05-29 19:27:17 -0300

    feat: lab-banco-digital-oo/

---
+++ lab-banco-digital-oo/src/Program/Main.java
@@ -0,0 +1,25 @@
+package Program;
+
+import Entities.Cliente;
+import Entities.Conta;
+import Entities.ContaCorrente;
+import Entities.ContaPoupanca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
+	}
+
+}
